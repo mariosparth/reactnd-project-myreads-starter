fbb88e6c2c9c041ffb8f3e6296ae4bdeb3e711d7
Marios P <marios_part@hotmail.com>
2017-11-12 19:15:06 +0200

    Add Link for routing buttons

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import Route from 'react-router-dom'
 import PropTypes from 'prop-types'
-
+import { Link } from 'react-router-dom'
 
 class ListBooks extends Component {
   render() {
@@ -163,7 +163,7 @@ class ListBooks extends Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to="/search">Add a book</Link>
         </div>
       </div>
 
