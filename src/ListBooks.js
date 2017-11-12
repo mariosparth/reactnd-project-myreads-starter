cff36685dc8e5976378a2353431b36fd73f02412
Marios P <marios_part@hotmail.com>
2017-11-13 00:09:07 +0200

    Filter first and then map results

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -8,31 +8,34 @@ class ListBooks extends Component {
   render() {
     console.log(this.props.books)
 
-    // state = {
-    //   shelf: ''
-    // }
-
     let showingBooks = this.props.books
+
+    const currentlyReading = showingBooks.filter((book) => book.shelf === 'currentlyReading')
+    const wantToRead = showingBooks.filter((book) => book.shelf === 'wantToRead')
+    const read = showingBooks.filter((book) => book.shelf === 'read')
+
+
+
 
     return (
 
       <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
+
+
         </div>
 
         <div className="list-books-content">
           <div>
 
 
-
             <div className="bookshelf">
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  {showingBooks.map((book) => (
+                  {currentlyReading.map((book) => (
                     <li key={book.id}>
-                      {book.shelf === 'currentlyReading' &&
                         <div className="book">
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
@@ -43,7 +46,6 @@ class ListBooks extends Component {
                           <div className="book-title">{book.title}</div>
                           <div className="book-authors">{book.authors.map((book) => (`${book}, `) ) }</div>
                         </div>
-                        }
                     </li>
                   ))}
                 </ol>
@@ -55,9 +57,8 @@ class ListBooks extends Component {
               <h2 className="bookshelf-title">Want to Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  {showingBooks.map((book) => (
+                  {wantToRead.map((book) => (
                     <li key={book.id}>
-                      {book.shelf === 'wantToRead' &&
                         <div className="book">
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
@@ -68,7 +69,6 @@ class ListBooks extends Component {
                           <div className="book-title">{book.title}</div>
                           <div className="book-authors">{book.authors.map((book) => (`${book}, `) ) }</div>
                         </div>
-                        }
                     </li>
                   ))}
                 </ol>
@@ -78,9 +78,8 @@ class ListBooks extends Component {
               <h2 className="bookshelf-title">Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  {showingBooks.map((book) => (
+                  {read.map((book) => (
                     <li key={book.id}>
-                      {book.shelf === 'read' &&
                         <div className="book">
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
@@ -91,7 +90,6 @@ class ListBooks extends Component {
                           <div className="book-title">{book.title}</div>
                           <div className="book-authors">{book.authors.map((book) => (`${book}, `) ) }</div>
                         </div>
-                        }
                     </li>
                   ))}
                 </ol>
